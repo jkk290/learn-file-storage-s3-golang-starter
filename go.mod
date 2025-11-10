331addb707b05f9183a20718c39f552cec408e22
jkk290 <jared@kuromoto.dev>
2025-11-10 13:34:48 -1000

    Finish AWS S3 chapter

--- go.mod
+++ go.mod
@@ -17,7 +17,7 @@ require (
 require (
 	github.com/aws/aws-sdk-go-v2 v1.39.6 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.7.3 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.31.17 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.31.17
 	github.com/aws/aws-sdk-go-v2/credentials v1.18.21 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.13 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.13 // indirect
@@ -28,7 +28,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.9.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.13 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.13 // indirect
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.90.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.90.0
 	github.com/aws/aws-sdk-go-v2/service/sso v1.30.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.35.5 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.39.1 // indirect
--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -43,4 +51,64 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	videoFormFile, _, formErr := r.FormFile("video")
+	if formErr != nil {
+		respondWithError(w, http.StatusInternalServerError, "error getting formfil", err)
+		return
+	}
+	defer videoFormFile.Close()
+
+	mediaType, _, parseErr := mime.ParseMediaType("video/mp4")
+	if parseErr != nil {
+		respondWithError(w, http.StatusInternalServerError, "error parsing media type", err)
+		return
+	}
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "invalid video format", err)
+		return
+	}
+	tempUploadFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error creating temp file", err)
+		return
+	}
+	defer os.Remove(tempUploadFile.Name())
+	defer tempUploadFile.Close()
+	_, copyErr := io.Copy(tempUploadFile, videoFormFile)
+	if copyErr != nil {
+		respondWithError(w, http.StatusInternalServerError, "error writing to temp file", err)
+		return
+	}
+	tempUploadFile.Seek(0, io.SeekStart)
+
+	randSlice := make([]byte, 32)
+	_, readErr := rand.Read(randSlice)
+	if readErr != nil {
+		respondWithError(w, http.StatusInternalServerError, "error creating random bytes", err)
+		return
+	}
+	randString := base64.RawURLEncoding.EncodeToString(randSlice)
+	videoFileName := randString + ".mp4"
+
+	_, uploadErr := cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         &videoFileName,
+		Body:        tempUploadFile,
+		ContentType: &mediaType,
+	})
+	if uploadErr != nil {
+		respondWithError(w, http.StatusInternalServerError, "error uploading to s3 bucket", err)
+	}
+	videoUrl := "https://" + cfg.s3Bucket + ".s3." + cfg.s3Region + ".amazonaws.com/" + videoFileName
+
+	if err := cfg.db.UpdateVideo(database.Video{
+		ID:                videoId,
+		CreatedAt:         dbVideoMetadata.CreatedAt,
+		UpdatedAt:         time.Now(),
+		ThumbnailURL:      dbVideoMetadata.ThumbnailURL,
+		VideoURL:          &videoUrl,
+		CreateVideoParams: dbVideoMetadata.CreateVideoParams,
+	}); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error saving to db", err)
+	}
 }
